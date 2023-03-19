e2cd50bf23c82ae557a7bb5185a40345f72b610f
Todd Hetrick <me@toddhetrick.com>
2023-03-19 10:41:03 -0400

    Some simple tcp listening

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        
+
+        println!("Connection established! {}", stream.peer_addr().unwrap());
+    }
 }
