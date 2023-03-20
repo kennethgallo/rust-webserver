e47733bd0f43e7c1f86e1519fe4ca12c0f630978
calvinhathcock <calvinhathcock@yahoo.com>
2023-03-20 10:05:01 -0400

    added example function for handling connection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
-use std::net::TcpListener;
+/*
+    Starter code for opening a TCP socket
+    and handling connections in Rust. 
+
+    Source: https://doc.rust-lang.org/book/ch20-01-single-threaded.html
+*/
+
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
-        
 
-        println!("Connection established! {}", stream.peer_addr().unwrap());
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
