b9cc4c9d828252abc413375e5c8a397483795a3c
Todd Hetrick <thetrick@uncc.edu>
2023-03-25 11:54:41 -0400

    brand for redcing header

--- src/handlers.rs
+++ src/handlers.rs
@@ -5,7 +5,9 @@ use std::net::{TcpStream};
 struct Request {
     method: String,
     path: String,
-    headers: Vec<(String, String)>,
+    host: String,
+    user-agent: String,
+    //headers: Vec<(String, String)>,
 }
 
 pub fn http_handler(mut stream: TcpStream) {
@@ -17,10 +19,10 @@ pub fn http_handler(mut stream: TcpStream) {
     let mut lines = request_str.lines();
 
     // Parse the first line of the request
-    let request_line = lines.next().unwrap();
-    let mut parts = request_line.split_whitespace();
-    let method = parts.next().unwrap().to_string();
-    let path = parts.next().unwrap().to_string();
+    //let request_line = lines.next().unwrap();
+    //let mut parts = request_line.split_whitespace();
+    //let method = parts.next().unwrap().to_string();
+    //let path = parts.next().unwrap().to_string();
 
     // Parse the headers
     for line in lines {
@@ -30,19 +32,20 @@ pub fn http_handler(mut stream: TcpStream) {
         let mut parts = line.splitn(2, ':');
         let name = parts.next().unwrap().to_string();
         let value = parts.next().unwrap().to_string();
-        headers.push((name, value));
+        println("Header name:{} val{}",name,val);
+        //headers.push((name, value));
     }
 
-    let request = Request {
-        method,
-        path,
-        headers,
-    };
+    //let request = Request {
+    //    method,
+     //   path,
+        //headers,
+    //};
     println!("{:#?}", request);
 
     // Respond to the client
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    //let response = "HTTP/1.1 200 OK\r\n\r\n";
+    //stream.write(response.as_bytes()).unwrap();
+    //stream.flush().unwrap();
 }
 
